36eb4ac18546cf53c7ed728b6ad08895b3a5c5cf
Marcelo <marcelosm@gmail.com>
2012-12-03 15:54:03 -0800

    test 2

--- lbreinvtool.go
+++ lbreinvtool.go
@@ -8,18 +8,18 @@ import (
 )
 
 type NMAPRun struct {
-	XMLName xml.Name `xml:"nmaprun"`
+	//XMLName xml.Name `xml:"nmaprun"`
 	Scanner string   `xml:"scanner,attr"`
 	Args    string   `xml:"args,attr"`
 	Start uint64 `xml:"start,attr"`
 	Version string `xml:"version,attr"`
 	XMLOutputVersion string `xml:"xmloutputversion,attr"`
 	ScanInfo ScanInfo `xml:"scaninfo"`
-	Hosts []Host `xml:"host"`
+	Host []Host `xml:"host"`
 }
 
 type ScanInfo struct {
-	XMLName xml.Name `xml:"scaninfo"`
+	//XMLName xml.Name `xml:"scaninfo"`
 	Type string `xml:"type,attr"`
 	Protocol string `xml:"protocol,attr"`
 	NumServices uint64 `xml:"numservices,attr"`
@@ -27,21 +27,23 @@ type ScanInfo struct {
 }
 
 type Host struct {
-	XMLName xml.Name `xml:"host"`
+	//XMLName xml.Name `xml:"host"`
 	StartTime uint64 `xml:"starttime,attr"`
 	EndTime uint64 `xml:"endtime,attr"`
-	Status Status
+	Status Status `xml:"status"`
 }
 
 type Status struct{
-	XMLName xml.Name `xml:"status"`
+	//XMLName xml.Name `xml:"status"`
 	State string `xml:"state,attr"`
 	Reason string `xml:"reason,attr"`
 }
 
 func main() {
-	xmlData, err := ioutil.ReadFile("sample-multiple.xml")
-	//xmlData, err := ioutil.ReadFile("sample-single.xml")
+	//xmlData, err := ioutil.ReadFile("full.xml")
+	//xmlData, err := ioutil.ReadFile("multiple.xml")
+	xmlData, err := ioutil.ReadFile("single.xml")
+
 	if err != nil {
 		fmt.Println("Error opening/reading file:", err)
 		os.Exit(1)
@@ -52,7 +54,4 @@ func main() {
 	xml.Unmarshal(xmlData, &n)
 
 	fmt.Printf("%+v\n",n)
-	//	for _, episode := range q.EpisodeList {
-	//		fmt.Printf("\t%s\n", episode)
-	//	}
 }
